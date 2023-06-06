89b990e580279edeb272ea77647917fc8f6ecd94
João Filipi <jf.britto@hotmail.com>
2023-06-06 01:50:21 -0300

    Melhorias

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <title>Indscraping @if (!empty($titulo)) | {{ $titulo }} @endif </title>
     
     <meta name="theme-color" content="#010005">
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
